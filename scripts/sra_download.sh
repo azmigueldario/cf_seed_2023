da607ad9f86cf949fa79ce4fe4df26c4bc0938ed
Miguel Prieto <mprietog@sfu.ca>
2023-03-20 20:08:56 +0000

    updated accession for CF

--- scripts/sra_download.sh
+++ scripts/sra_download.sh
@@ -12,3 +12,31 @@ module load singularity
 
 # path to SRA_tools container
 SRA_IMG="/project/60005/cidgoh_share/singularity_imgs/sra-tools_3.0.0.sif"
+
+ACC_LIST="/project/60005/mdprieto/cf_seed_2023/ncbi_accessions/trial_accessions.txt"
+
+SCRIPTS="/project/60005/mdprieto/cf_seed_2023/scripts"
+
+
+singularity run -B /scratch,/project "$SRA_IMG" prefetch \
+    --option-file "$ACC_LIST" \
+    --type fastq \
+    -O "/project/60005/mdprieto/cf_seed_2023/raw_data" \
+    --resume yes
+
+singularity run -B /scratch,/project "$SRA_IMG" fasterq-dump \
+    /project/60005/mdprieto/cf_seed_2023/raw_data                          `# dir with prefetched data` \
+    -O "/project/60005/mdprieto/cf_seed_2023/raw_data"                      `# output dir`
+
+# worked for trial produces only fastq
+singularity run -B /scratch,/project "$SRA_IMG" fasterq-dump ERR*\
+    -O "/project/60005/mdprieto/cf_seed_2023/raw_data"                      `# output dir`
+
+cat "$ACC_LIST" | \
+     xargs singularity run -B /scratch,/project "$SRA_IMG" fasterq-dump \
+    -O "/project/60005/mdprieto/cf_seed_2023/raw_data"                              `# output dir`
+
+cat "$ACC_LIST" | \
+     xargs singularity run -B /scratch,/project "$SRA_IMG" fasterq-dump \
+     --include-technical -S \
+    -O "/project/60005/mdprieto/cf_seed_2023/raw_data"                              `# output dir`
