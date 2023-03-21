0a7f06dfea12bf1bde240e8bb48a4a0ddf688e53
Miguel Prieto <mprietog@sfu.ca>
2023-03-21 00:05:58 +0000

    working script for datasets download

--- scripts/fetch_ngs.sh
+++ scripts/fetch_ngs.sh
@@ -1,34 +1,29 @@
-#!/bin/bash                                 # she bang
+#!/bin/bash                                 
 #SBATCH --mem-per-cpu=6G                    # GB of memory per cpu core - max 120GBper node
-#SBATCH --time=00:10:00                     # walltime
+#SBATCH --time=03:10:00                     # walltime
 #SBATCH --cpus-per-task=4                   # CPU cores per task (multithread) - Max 8 
 #SBATCH --job-name="sra_download"           # job_name (optional)
 #SBATCH --chdir=/scratch/mdprieto/          # change directory before executing optional)
-#SBATCH --output=giardia_download_data.out       # output_file specification (optional)
+#SBATCH --output=cfseed_download_data.out       # output_file specification (optional)
 
 ######################################################################################################
 
 # load necessary modules
 module load singularity nextflow
 
-# path to SRA_tools container
-SRA_IMG="/project/60005/cidgoh_share/singularity_imgs/sra-tools_3.0.0.sif"
-# path to accession list
-ACC_LIST="/project/60005/mdprieto/cf_seed_2023/ncbi_accessions/trial_accessions.csv"
+# ENV variables
+NCFB_ACC="/project/60005/mdprieto/cf_seed_2023/ncbi_accessions/ncfb_PRJNA590225.csv"
+CF_ACC="/project/60005/mdprieto/cf_seed_2023/ncbi_accessions/cf_PRJNA516870.csv"
+DATA_DIR="/project/60005/mdprieto/cf_seed_2023/raw_data"
 
-# if necessary to prepare all dependencies
-nf-core download fetchngs \
-    --container singularity \
-    --outdir "/scratch/mdprieto/nfcore-fetchngs-1.9" \
-    --singularity-cache-only
-
-# test run from nf-core
-nextflow run nf-core/fetchngs \
-    -profile test,singularity \
-    --outdir /project/60005/mdprieto/cf_seed_2023/raw_data
-
-# test run with my data
+# download NCFB
 nextflow run nf-core/fetchngs -r 1.9 \
-    --input "$ACC_LIST" \
+    --input "$NCFB_ACC" \
     -profile singularity \
-    --outdir /project/60005/mdprieto/cf_seed_2023/raw_data
+    --outdir $DATA_DIR
+
+# download CF
+nextflow run nf-core/fetchngs -r 1.9 \
+    --input "$CF_ACC" \
+    -profile singularity \
+    --outdir $DATA_DIR
