8c5e6b212db2e7c4b235034e23cf90c96f5ca1f2
Miguel Prieto <mprietog@sfu.ca>
2023-03-24 18:40:31 +0000

    updated script to download data

--- scripts/fetch_ngs.sh
+++ scripts/fetch_ngs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                                 
 #SBATCH --mem-per-cpu=6G                    # GB of memory per cpu core - max 120GBper node
-#SBATCH --time=03:10:00                     # walltime
+#SBATCH --time=04:30:00                     # walltime
 #SBATCH --cpus-per-task=4                   # CPU cores per task (multithread) - Max 8 
 #SBATCH --job-name="sra_download"           # job_name (optional)
 #SBATCH --chdir=/scratch/mdprieto/          # change directory before executing optional)
@@ -20,10 +20,12 @@ DATA_DIR="/project/60005/mdprieto/cf_seed_2023/raw_data"
 nextflow run nf-core/fetchngs -r 1.9 \
     --input "$NCFB_ACC" \
     -profile singularity \
-    --outdir $DATA_DIR
+    -resume \
+    --outdir $DATA_DIR/ncfb_data
 
 # download CF
 nextflow run nf-core/fetchngs -r 1.9 \
     --input "$CF_ACC" \
     -profile singularity \
-    --outdir $DATA_DIR
+    -resume \
+    --outdir $DATA_DIR/cf_data
