774b324b199ef136c7c1f07b7d9568ad78165b3c
Miguel Prieto <mprietog@sfu.ca>
2023-03-28 00:53:17 +0000

    added taxprofiler script for cf reads

--- scripts/nfcore_taxprof_cf.sh
+++ scripts/nfcore_taxprof_cf.sh
@@ -1,10 +1,12 @@
 #!/bin/bash                                 
-#SBATCH --mem-per-cpu=10G                           # GB of memory per cpu core - max 120GBper node
-#SBATCH --time=12:30:00                             # walltime
-#SBATCH --cpus-per-task=10                           # CPU cores per task (multithread) - Max 8 
-#SBATCH --job-name="taxprof_pipeline_ncfb"              # job_name (optional)
-#SBATCH --chdir=/scratch/mdprieto/                  # change directory before executing optional)
-#SBATCH --output=./jobs_output/cfseed_taxprof_ncfb.out      
+#SBATCH --mem-per-cpu=11G                 
+#SBATCH --ntasks=2
+#SBATCH --nodes=2
+#SBATCH --time=01:30:00                             
+#SBATCH --cpus-per-task=8                         
+#SBATCH --job-name="taxprof_pipeline_cf"            
+#SBATCH --chdir=/scratch/mdprieto/                  
+#SBATCH --output=./jobs_output/taxprof_cf_cfseed.out      
 
 ######################################################################################################
 
@@ -14,7 +16,7 @@ module load singularity nextflow
 # ENV variables
 
 KRAKEN2_DB="/home/mdprieto/object_database/kraken2/k2_standard_20221209.tar.gz"
-SAMPLE_SHEET="/project/60005/mdprieto/cf_seed_2023/scripts/samplesheet_taxprof_ncfb.csv"
+SAMPLE_SHEET_CF="/project/60005/mdprieto/cf_seed_2023/scripts/samplesheet_taxprof_cf.csv"
 DB_CSV="/project/60005/mdprieto/cf_seed_2023/scripts/databases_taxprof.csv"
 
 
@@ -23,14 +25,14 @@ DB_CSV="/project/60005/mdprieto/cf_seed_2023/scripts/databases_taxprof.csv"
 nextflow run nf-core/taxprofiler -r 1.0.0 \
     -profile singularity \
     -resume \
-    --input $SAMPLE_SHEET \
+    --input $SAMPLE_SHEET_CF \
     --databases $DB_CSV \
-    --outdir /scratch/mdprieto/cf_seed_results/taxprof \
+    --outdir /scratch/mdprieto/cf_seed_results/taxprof/cf \
     --perform_shortread_qc \
     --perform_shortread_hostremoval \
     --hostremoval_reference /project/cidgoh-object-storage/database/reference_genomes/human/GRCh38.p14/GCF_000001405.40/GCF_000001405.40_GRCh38.p14_genomic.fna \
     --shortread_hostremoval_index /project/cidgoh-object-storage/database/bowtie_GRCh38 \
     --run_bracken \
     --run_kraken2 \
-    --max_cpus 10 \
-    --max_memory 95GB
+    --max_cpus 8 \
+    --max_memory 85GB
